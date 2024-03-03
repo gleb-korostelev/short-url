e333d07111873b02bb74b2c1ef2db637daa7dcb1
gleb-korostelev <sungvin99@mail.ru>
2024-03-03 19:44:40 +0400

    gzip middleware

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -1,7 +1,10 @@
 package middleware
 
 import (
+	"compress/gzip"
+	"io"
 	"net/http"
+	"strings"
 	"time"
 
 	"go.uber.org/zap"
@@ -25,6 +28,36 @@ func LoggingMiddleware(next http.HandlerFunc, logger *zap.Logger) http.HandlerFu
 	})
 }
 
+func GzipCompressMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
+			gzWriter := gzip.NewWriter(w)
+			defer gzWriter.Close()
+
+			w.Header().Set("Content-Encoding", "gzip")
+			next.ServeHTTP(gzipResponseWriter{Writer: gzWriter, ResponseWriter: w}, r)
+		} else {
+			next.ServeHTTP(w, r)
+		}
+	})
+}
+
+func GzipDecompressMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Header.Get("Content-Encoding") == "gzip" {
+			gzReader, err := gzip.NewReader(r.Body)
+			if err != nil {
+				http.Error(w, "Failed to create gzip reader", http.StatusBadRequest)
+				return
+			}
+			defer gzReader.Close()
+			r.Body = gzReader
+			r.Header.Del("Content-Encoding")
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
 type responseWriter struct {
 	http.ResponseWriter
 	status int
@@ -43,4 +76,13 @@ func (w *responseWriter) Write(b []byte) (int, error) {
 	size, err := w.ResponseWriter.Write(b)
 	w.size += size
 	return size, err
+}
+
+type gzipResponseWriter struct {
+	io.Writer
+	http.ResponseWriter
+}
+
+func (w gzipResponseWriter) Write(b []byte) (int, error) {
+	return w.Writer.Write(b)
 }
--- internal/service/router/router.go
+++ internal/service/router/router.go
@@ -9,6 +9,8 @@ import (
 
 func RouterInit(logger *zap.Logger) *chi.Mux {
 	router := chi.NewRouter()
+	router.Use(middleware.GzipCompressMiddleware)
+	router.Use(middleware.GzipDecompressMiddleware)
 	router.Get("/{id}", middleware.LoggingMiddleware(handler.GetOriginal, logger))
 	router.Post("/", middleware.LoggingMiddleware(handler.PostShorter, logger))
 	router.Post("/api/shorten", middleware.LoggingMiddleware(handler.PostShorterJSON, logger))
